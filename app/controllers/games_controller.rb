a8ac9d845934022d55f23e84bde5b3766716e84e
Oskar Niburski <oskarniburski@gmail.com>
2016-06-29 19:21:03 -0400

    Show for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,44 +1,8 @@
 class GamesController < ApplicationController
   before_action :set_game, only: [:show, :update, :destroy]
-
+  before_action :all_games, only: [:index]
   # GET /games
   def index
-    @games = {data: [
-              {
-                id: '1', 
-                type: 'games',
-                attributes: {
-                  name: 'Tic Tac Toe', 
-                  num_players: 2
-                }
-              },
-              {
-                id: '2', 
-                type: 'games',
-                attributes: {
-                  name: 'Connect Four', 
-                  num_players: 2
-                }
-              },
-              {
-                id: '3', 
-                type: 'games',
-                attributes: {
-                  name: 'Rock Paper Scissors', 
-                  num_players: 2
-                }
-              },
-              {
-                id: '4', 
-                type: 'games',
-                attributes: {
-                  name: 'Stix', 
-                  num_players: 2
-                }
-              }
-          ]
-      }
-
     render json: @games
   end
 
@@ -75,11 +39,49 @@ class GamesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_game
-      @game = Game.find(params[:id])
+      @games = all_games().first[1]
+      @game = @games.find{|e| e[:id] == params[:id]}
+      @game = {data: @game}
     end
 
     # Only allow a trusted parameter "white list" through.
     def game_params
       params.fetch(:game, {})
+    end
+
+    def all_games 
+      @games = {data: [{
+                id: '1', 
+                type: 'games',
+                attributes: {
+                  name: 'Tic Tac Toe', 
+                  num_players: 2
+                }
+              },
+              {
+                id: '2', 
+                type: 'games',
+                attributes: {
+                  name: 'Connect Four', 
+                  num_players: 2
+                }
+              },
+              {
+                id: '3', 
+                type: 'games',
+                attributes: {
+                  name: 'Rock Paper Scissors', 
+                  num_players: 2
+                }
+              },
+              {
+                id: '4', 
+                type: 'games',
+                attributes: {
+                  name: 'Stix', 
+                  num_players: 2
+                }
+              }]
+            }
     end
 end
